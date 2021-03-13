a80003f070ad723bf0885afc34a0025bb9cb7ebd
Berk <70212751+BerkTas20@users.noreply.github.com>
2021-03-13 23:41:50 +0300

    Add project files.

---
+++ SampleDictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> professions = new MyList<string>();
+            professions.Add("Programmer");
+            Console.WriteLine(professions.Length);
+
+            professions.Add("Academician");
+            Console.WriteLine(professions.Length);
+
+
+            professions.Add("Translator");
+            Console.WriteLine(professions.Length);
+
+
+            professions.Add("Administrative assistant");
+            Console.WriteLine(professions.Length);
+
+
+            professions.Add("Doctor");
+            Console.WriteLine(professions.Length);
+
+            foreach (var x in professions.Items)
+            {
+                Console.WriteLine(x);
+            }
+            
+        }
+
+
+        
+    }
+}
---
+++ SampleDictionary2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleDictionary2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList2<string> fighters = new MyList2<string>();
+
+            fighters.Add("'1' George 'Rush' Saint Pierre >> Middleweight >> 26-2-0");
+            Console.WriteLine(fighters.Length);
+
+
+            fighters.Add("'2'Jon 'Bones' Jones  >> Light Heavyweight >> 26-1-0");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'3' Khabib 'The Eagle' Nurmagomedov  >> Lightweight >> 29-0-0");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'4' Anderson 'The Spider' Silva >> Middleweight >> 34-11-0");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'5' Demetrious 'Might Mouse' Johson >> Bantamweight >> 30-3-1");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'6' Fedor Emelianenko >> Heavyweight >> 39-6-0");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'7' Stipe Miocic >> Heavyweight >> 20-3-0");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'8' Daniel Cormier >> Heavyweight >> 22-3-0");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'9' Dustin Poirier >> Lightweight >> 27-6-0");
+            Console.WriteLine(fighters.Length);
+
+            fighters.Add("'10' Conor McGregor >> Lightweight >> 22-5-0");
+            Console.WriteLine(fighters.Length);
+
+            Console.WriteLine("<<<<<<<<<<BEST UFC FİGHTERS OF ALL TİME>>>>>>>>> ");
+
+
+            foreach (var x in fighters.Items)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
